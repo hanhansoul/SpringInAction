16f61d6cec98baf337242589932b62959f6499b9
hanhansoul <whr2290@163.com>
2018-06-14 19:58:00 +0800

    spring security demo init

---
+++ SpringSecurityDemo/src/main/java/com/master/SpringSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDemoApplication.class, args);
+	}
+}
